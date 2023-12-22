1a1750f915df1e669058f4233ffb0e787293bcf1
r1kk1s <110721833+r1kk1s@users.noreply.github.com>
2023-12-22 12:22:52 +0300

    Update main.py

--- async_write_data_to_sqlite/main.py
+++ async_write_data_to_sqlite/main.py
@@ -10,7 +10,7 @@ from csv_work import PATH_TO_FILES, FILE_NUMBERS, create_csv_files
 from db_work import INSERT_QUERY, PATH_TO_DB, DB, create_table
 
 
-async def gen_async_csv_data_iterator(num: int) -> AsyncGenerator[list[str], None]:
+async def gen_async_csv_data(num: int) -> AsyncGenerator[list[str], None]:
     async with aiofiles.open(f"{PATH_TO_FILES}/csv_file_{num}.csv", "r") as file:
         async_csv_data_iterator = AsyncReader(file)
         await async_csv_data_iterator.__anext__()
@@ -22,7 +22,7 @@ async def db_write(numbers: int) -> None:
     t = pc()
     with sqlite3.connect(f"{PATH_TO_DB}/{DB}") as conn:
         for num in range(numbers):
-            async for reader in gen_async_csv_data_iterator(num):
+            async for reader in gen_async_csv_data(num):
                 conn.execute(INSERT_QUERY, reader)
         conn.commit()
     print(pc() - t)
